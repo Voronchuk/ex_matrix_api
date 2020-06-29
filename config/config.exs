acb8967e6b81e721a961d6d471f550079820f7e0
Vyacheslav Voronchuk <voronchuk@gmail.com>
2020-06-29 12:22:43 +0300

    Changed config resolution

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :ex_matrix_api,
+config :ex_matrix_api, ExMatrixApi.Synapse,
   http_client: ExMatrixApi.Util.HttpClient.HTTPoison
 
 if File.exists?("config/config.secret.exs") do
